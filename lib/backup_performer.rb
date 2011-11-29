1c2467d1bf66093140458dcbb453266b3e8505eb
Matt Simpson <matt.simpson3@gmail.com>
2011-11-28 21:58:09 -0600

    Add backup performer

---
+++ lib/backup_performer.rb
@@ -0,0 +1,21 @@
+class BackupPerformer
+  def self.backup_location
+
+  end
+
+  def initialize(global_options, options, args)
+
+  end
+
+  def ready?
+
+  end
+
+  def call
+
+  end
+
+  def errors
+
+  end
+end
---
+++ spec/lib/backup_performer_spec.rb
@@ -0,0 +1,5 @@
+require_relative '../../lib/backup_performer'
+
+describe BackupPerformer do
+  
+end
